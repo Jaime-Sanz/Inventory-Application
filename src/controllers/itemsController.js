e991cb8bc3e8462ef6e2e05febd9e114e4333fde
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2025-03-14 21:28:34 -0700

    Added description column to games table, created homepage and create page EJS files (CSS WIP)

--- src/controllers/itemsController.js
+++ src/controllers/itemsController.js
@@ -1,7 +1,19 @@
-export function getAllItemsController(req, res) {
-    res.render();
+import pool from '../db/pool.js';
+
+export const getAllItemsController = (req, res) => {
+    res.render('homepage', {title: 'Rated Games'});
 }
 
-export function getCreateItemsController(req, res) {
-    res.render();
+export const getCreateItemsController = (req, res) => {
+    res.render('createpage', {title: 'Add Game'});
+}
+
+export const postCreateItemsController = async (req, res) => {
+    try {
+        const { vg_name, vg_description, vg_rating, vg_price, vg_stock } = req.body;
+
+        const result = await pool.query(
+            'INSERT INTO '
+        )
+    }
 }
--- src/routes/itemsRoute.js
+++ src/routes/itemsRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllItemsController, getCreateItemsController } from '../controllers/itemsController.js';
+import { getAllItemsController, getCreateItemsController, postCreateItemsController } from '../controllers/itemsController.js';
 
 const router = express.Router();
 
 router.get('/', getAllItemsController);
 router.get('/create', getCreateItemsController);
+router.post('/create', postCreateItemsController);
 
 export default router;
